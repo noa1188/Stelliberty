2436be0515887268d14e645c4b6bfbe4fd0e60ab
ONEUI9 <oneui9@example.com>
2026-01-16 18:53:30 +0800

    fix(system-proxy): 简化 PAC 脚本

--- lib/storage/clash_preferences.dart
+++ lib/storage/clash_preferences.dart
@@ -457,17 +457,7 @@ class ClashPreferences {
     final proxyHost = getProxyHost();
     final proxyPort = getMixedPort();
     return '''function FindProxyForURL(url, host) {
-    // 本地地址直连
-    if (isPlainHostName(host) ||
-        shExpMatch(host, "*.local") ||
-        isInNet(dnsResolve(host), "10.0.0.0", "255.0.0.0") ||
-        isInNet(dnsResolve(host), "172.16.0.0", "255.240.0.0") ||
-        isInNet(dnsResolve(host), "192.168.0.0", "255.255.0.0") ||
-        isInNet(dnsResolve(host), "127.0.0.0", "255.0.0.0"))
-        return "DIRECT";
-    
-    // 其他地址使用代理
-    return "PROXY $proxyHost:$proxyPort";
+    return "PROXY $proxyHost:$proxyPort; SOCKS5 $proxyHost:$proxyPort; DIRECT";
 }''';
   }
 
